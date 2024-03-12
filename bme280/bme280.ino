629442215bfe415a9c91836f7005c5722a3d12e7
Juan Manuel Mejia Pérez <jmejiap1600@alumno.ipn.mx>
2024-03-12 00:58:29 -0600

    Se agrega el código para implementar el BME280

--- bme280/bme280.ino
+++ bme280/bme280.ino
@@ -7,6 +7,11 @@ Adafruit_BME280 bme;  //Se crea el objeto BME280
 
 //Prototitpo de funciones
 void bme_verify();
+void bme_read();
+
+//Variables
+float T, P, P0, h = 0.0;
+int H = 0;
 
 void setup() {
   //Inicialización de la terminal serie
@@ -19,8 +24,25 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bme_read();
 
+  Serial.print("Presion: ");
+  Serial.print(P);
+  Serial.print(" hPa ");
+  
+  Serial.print("Temperatura: ");
+  Serial.print(T);
+  Serial.print(" C ");
+  
+  Serial.print("Humedad: ");
+  Serial.print(H);
+  Serial.print(" % ");
+  
+  Serial.print("Altitud: ");
+  Serial.print(h);
+  Serial.println(" m");
+
+  delay(5000);
 }
 
 void bme_verify(){
@@ -28,7 +50,18 @@ void bme_verify(){
   if (!bme.begin(BME280_ADDRESS )) {
     Serial.println("S01");
   }
-  Serial.println("Online");
-  Serial.println()
-  delay(1000ms)
+  else{
+    P0=bme.readPressure()/100; //Se lee la presión de referencia [hPa]
+    Serial.println("Online");
+    Serial.print(P0);
+    Serial.println(" hPa");
+    delay(1000);
+  }
+}
+
+void bme_read(){
+  P = bme.readPressure()/100;   //Se lee la presión [hPa]
+  T = bme.readTemperature();    //Se lee la temperatura [°C]
+  H = bme.readHumidity();       //Se lee la humedad relativa [%]
+  h = bme.readAltitude(P0);     //Se calcula la altitud [m]
 }
